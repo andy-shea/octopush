cf0d0bfdb3e7729fa03d480f65c7a6e705d715a0
Andy Shea <andrew@octahedron.com.au>
2016-10-10 23:18:55 +0100

    Simplified knex config

--- knexfile.js
+++ knexfile.js
@@ -1,50 +1,14 @@
 var config = require('config');
 
 module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: {
-      host: config.db.host,
-      database: config.db.name,
-      user: config.db.user,
-      password: config.db.password
-    },
-    migrations: {
-      tableName: 'migrations'
-    }
+  client: 'postgresql',
+  connection: {
+    host: config.db.host,
+    database: config.db.name,
+    user: config.db.user,
+    password: config.db.password
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      host: config.db.host,
-      database: config.db.name,
-      user: config.db.user,
-      password: config.db.password
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      host: config.db.host,
-      database: config.db.name,
-      user: config.db.user,
-      password: config.db.password
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'migrations'
-    }
+  migrations: {
+    tableName: 'migrations'
   }
 };
