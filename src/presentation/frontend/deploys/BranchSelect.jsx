b46ec12e81ec0de553456b3c76e32fd49ddbaa77
Andy Shea <andrew@octahedron.com.au>
2016-12-16 09:30:42 +0000

    Handles empty repo branches gracefully

--- src/presentation/frontend/deploys/BranchSelect.jsx
+++ src/presentation/frontend/deploys/BranchSelect.jsx
@@ -4,7 +4,7 @@ import Select from 'react-select';
 import {deploySelect, branchSelect} from './DeploySettings.css';
 
 function BranchSelect({branches, selectBranch, selectedBranch}) {
-  const branchOptions = branches.map(branch => ({value: branch, label: branch}));
+  const branchOptions = branches && branches.map(branch => ({value: branch, label: branch}));
   return (
     <Select name="branch" instanceId="branch" clearable={false} options={branchOptions} placeholder="branch"
         className={cx(deploySelect, branchSelect)} value={selectedBranch} onChange={selectBranch}/>
