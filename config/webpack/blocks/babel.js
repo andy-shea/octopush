7a9d534bb708afd2097d6a6a297622d57b574f15
Andy Shea <aa.shea@gmail.com>
2018-09-06 23:00:12 +0100

    Setup typescript

--- config/webpack/blocks/babel.js
+++ config/webpack/blocks/babel.js
@@ -1,16 +1,26 @@
 function babel(options) {
-  return (context, {addLoader}) =>
-    addLoader(
-      Object.assign(
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: Object.assign({cacheDirectory: '/tmp', presets: ['@babel/preset-env']}, options)
-        },
-        context.match
-      )
-    );
+  return (context, {merge}) =>
+    merge({
+      resolve: {
+        extensions: ['ts', 'tsx']
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(j|t)sx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: Object.assign({cacheDirectory: '/tmp'}, options)
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ['source-map-loader'],
+            enforce: 'pre'
+          }
+        ]
+      }
+    });
 }
 
 module.exports = babel;
