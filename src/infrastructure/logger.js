3f0ede6159e8f6286112fafe1fbea1ac3b0822c0
Andy Shea <andy@tjs.com>
2019-02-11 11:48:20 +0000

    Move logging dev dependency to dev only code path

--- src/infrastructure/logger.js
+++ src/infrastructure/logger.js
@@ -1,12 +1,14 @@
 const bunyan = require('bunyan');
 const path = require('path');
-const PrettyStream = require('bunyan-prettystream');
 
-const streams = [{
-  level: (process.env.NODE_ENV === 'development') ? 'debug' : 'info',
-  path: path.join(__dirname, '/../../log/octopush')
-}];
+const streams = [
+  {
+    level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+    path: path.join(__dirname, '/../../log/octopush')
+  }
+];
 if (process.env.NODE_ENV === 'development') {
+  const PrettyStream = require('bunyan-prettystream');
   const prettyStdOut = new PrettyStream();
   prettyStdOut.pipe(process.stdout);
   streams.push({level: 'debug', type: 'raw', stream: prettyStdOut});
